045c19cb267d9e962f8f7f89b6e8fc857c07f867
Molly Seither <molly.seither@centre.edu>
2018-06-28 16:25:31 +0000

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
 function dwarfRollCall(dwarves) {
+  var numbered = []
+  for (var i=0;i<dwarves.length;i++){
+    numbered.push("${i+1}. {dwarves.[i] "}
+  return numbered.join("")
 }
 
 function summonCaptainPlanet(planeteerCalls){
+  return planeteerCalls.map(a=>a.toUpperCase()+"!")
 }
 
 function longPlaneteerCalls(words) {
+  for (var i=0;i<words.length;i++){
+    if (words[i].length>4){
+      return true}
+    }
+  return false
 }
 
 function findTheCheese (foods) {
+  for (var i=0;i<foods.lenth;i++)
+    if (foods[i]=="cheddar"||foods[i]=="gouda"||foods[i]=="camembert"){
+      return foods[i]}
+    }
+  return "no cheese!"
 }
